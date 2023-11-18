6c6fcb23acd80d1115f4c76530200a0b44bffd40
dzhuang725 <36779170+dzhuang725@users.noreply.github.com>
2023-11-18 13:36:47 -0500

    cleaned up entry points
    added "npm run develop"

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, console.log("Server has started on port: " + PORT));
 // const mongoose = require("mongoose");
 // mongoose.connect();
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
