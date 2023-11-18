96d620e8868c42c0e515de01e42d513e2f13d36d
dzhuang725 <36779170+dzhuang725@users.noreply.github.com>
2023-11-18 13:50:46 -0500

    set up database connection
    set up secure Mongodb connection with Mongoose and dotenv

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const database = process.env.MONGO_URI;
+mongoose
+  .connect(database, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
+  .then(() => console.log("db has connected"))
+  .catch((err) => console.log(err));
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, console.log("Server has started on port: " + PORT));
-// const mongoose = require("mongoose");
-// mongoose.connect();
