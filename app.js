62c6e1ab37767071fae1d3b64fc2a15015a76a83
dzhuang725 <36779170+dzhuang725@users.noreply.github.com>
2023-11-19 15:09:16 -0500

    changed index.js to app.js

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
 
-// Routes
-app.use("/", require("./routes/login"));
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, console.log("Server has started on port: " + PORT));
+// BodyParsing
+// app.use(express.urlencoded({ extended: false }));
 
 // Database
 const mongoose = require("mongoose");
@@ -20,3 +17,9 @@ mongoose
   })
   .then(() => console.log("db has connected"))
   .catch((err) => console.log(err));
+
+// Routes
+app.use("/", require("./routes/login"));
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, console.log("Server has started on port: " + PORT));
