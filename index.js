31faefe257e777e1bdbe1612f954c84928f0bbf2
dzhuang725 <36779170+dzhuang725@users.noreply.github.com>
2023-11-18 20:08:45 -0500

    added login and register page
    - created loginController.js to allow rendering register and login page
    - created login.js in routes
    - created FrontEnd with ejs
    - modified index.js to add login route

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+app.set("view engine", "ejs");
+
+// Routes
+app.use("/", require("./routes/login"));
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, console.log("Server has started on port: " + PORT));
+
+// Database
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-
 const database = process.env.MONGO_URI;
 mongoose
   .connect(database, {
@@ -12,6 +20,3 @@ mongoose
   })
   .then(() => console.log("db has connected"))
   .catch((err) => console.log(err));
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, console.log("Server has started on port: " + PORT));
