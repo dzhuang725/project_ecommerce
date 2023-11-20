10d61bdfb7fde8822ca47c6a818913abe80bffa2
dzhuang725 <36779170+dzhuang725@users.noreply.github.com>
2023-11-19 23:42:52 -0500

    finished add favorite functionality
    - tested with database

---
+++ controllers/favouritesController.js
@@ -0,0 +1,44 @@
+const User = require("../models/User");
+const Product = require("../models/Product");
+
+const addFavorite = async (req, res) => {
+  const productId = req.body.productId;
+  const userId = req.user.userId; // Assumed to be available via authentication middleware
+  try {
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).send("Product not found");
+    }
+
+    await User.findByIdAndUpdate(userId, {
+      $addToSet: { favorites: productId },
+    });
+    res.redirect("back"); // Redirect back to the page where the request was made
+  } catch (error) {
+    res.status(500).send("Error adding to favorites");
+  }
+};
+
+const removeFavorite = async (req, res) => {
+  const productId = req.params.productId;
+  const userId = req.user._id;
+
+  try {
+    await User.findByIdAndUpdate(userId, { $pull: { favorites: productId } });
+    res.redirect("back");
+  } catch (error) {
+    res.status(500).send("Error removing from favorites");
+  }
+};
+
+const getFavorites = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).populate("favorites");
+    res.json(user.favorites);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error fetching favorites");
+  }
+};
+
+module.exports = { addFavorite, removeFavorite, getFavorites };
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,23 @@
 var express = require("express");
 var router = express.Router();
 const productController = require("../controllers/productController");
+const favoritesController = require("../controllers/favouritesController");
 const isAuthenticated = require("../middleware/isAuthenticated");
 
 router.get("/", isAuthenticated, productController.getProducts);
 router.get("/products*", isAuthenticated, productController.getProducts);
+router.get("/favorites", isAuthenticated, favoritesController.getFavorites);
+
+router.post(
+  "/favorites/add/:productId",
+  isAuthenticated,
+  favoritesController.addFavorite
+);
+
+router.delete(
+  "/favorites/remove/:productId",
+  isAuthenticated,
+  favoritesController.removeFavorite
+);
 
 module.exports = router;
