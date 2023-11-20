7a0fa49cd3ee36ee24a24bad7d6ad2f0fae43c53
dzhuang725 <36779170+dzhuang725@users.noreply.github.com>
2023-11-19 22:32:34 -0500

    implemented filter on display products

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,11 +4,24 @@ const getProducts = async (req, res) => {
   const perPage = 9; // Number of products per page
   const page = parseInt(req.query.page) || 1; // Current page
 
+  const brandFilter = req.query.brand
+    ? { brand: { $in: req.query.brand } }
+    : {};
+  const typeFilter = req.query.type ? { type: { $in: req.query.type } } : {};
+
   try {
-    const products = await Product.find()
+    const products = await Product.find({ ...brandFilter, ...typeFilter })
       .skip(perPage * page - perPage)
       .limit(perPage);
-    const count = await Product.countDocuments(); // Total number of products
+
+    const count = await Product.countDocuments({
+      ...brandFilter,
+      ...typeFilter,
+    });
+
+    if (count === 0) {
+      return res.render("noProductsFound"); // Render a view for no products found
+    }
 
     res.render("home", {
       title: "Ecommerce app",
@@ -19,13 +32,6 @@ const getProducts = async (req, res) => {
       hasNextPage: perPage * page < count,
       hasPrevPage: page > 1,
     });
-    // res.render("productGallery", {
-    //   products: products,
-    //   currentPage: parseInt(page),
-    //   totalPages: Math.ceil(count / perPage),
-    //   hasNextPage: perPage * page < count,
-    //   hasPrevPage: page > 1,
-    // });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
